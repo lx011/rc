65908f13b87608ac101a1c2f38a090d3395c028c
Len <cxin1314@gmail.com>
2019-05-20 00:29:26 +0800

    chore: favicon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,8 +7,8 @@ interface ButtonProps {
 export default ({ text }: ButtonProps) => {
   const [count, setCount] = useState(0);
   const handleClick = () => {
-    const add = count + 1;
-    setCount(add);
+    const add = count + 1
+    setCount(add)
   }
   return (
     <div className="nui__btn" onClick={handleClick}>
